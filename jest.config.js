10db9f2ed3f530275f72a1c0b9cedc0b39e4d507
Arlo Armstrong <arlo.phoenix@gmail.com>
2019-02-20 20:30:21 +1300

    report junit results to circle

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 // https://github.com/iamturns/create-exposed-app/blob/master/jest.config.js
 module.exports = {
   collectCoverageFrom: ["src/**/*.{js,ts}", "!src/**/*.d.ts"],
+  reporters: ["default", "jest-junit"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
 };
